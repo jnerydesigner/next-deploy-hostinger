cfe316ad248c6e936c9874162a8277c8e6a21232
Jander Nery <jander.webmaster@gmail.com>
2024-03-05 15:10:25 -0400

    Texto Novo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,18 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>New Deploy in NextJS - By Jander Nery</h2>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque a
-        porro blanditiis, id reprehenderit expedita ducimus, harum ipsum dicta
-        odio veniam assumenda accusamus ipsam. Officia suscipit commodi libero
-        deserunt fugit.
+        O texto dissertativo – ou dissertação – é um tipo de texto, de natureza
+        teórica, que tem o objetivo principal de expor um tema. De forma geral,
+        um bom texto dissertativo compreende uma exposição minuciosa sobre o
+        tema. Por isso, é fundamental que a pessoa autora demostre ter
+        conhecimentos amplos sobre o assunto. A principal característica desse
+        tipo de texto é a capacidade de despertar reflexão em que está lendo.
+        Seja através das exposições ou dos argumentos. Ou seja: a intenção do
+        texto é gerar a capacidade de pensar sobre aquele assunto, a partir de
+        fatos ou opiniões. Isso faz com que o texto dissertativo seja necessário
+        em todos os níveis de escolaridade e com diferentes graus de
+        complexidade: do ensino médio ao mestrado e doutorado; das disciplinas
+        da escola até a prova do ENEM e vestibulares.
       </p>
     </main>
   );
