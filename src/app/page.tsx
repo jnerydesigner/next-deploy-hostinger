65b8f7b27053bb949452ab1f2ac3a8123445491d
root <root@srv483955.hstgr.cloud>
2024-03-05 19:04:16 +0000

    Test jenkins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>New Deploy in NextJS - By Jander Nery</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt blanditiis
-        facilis alias voluptate placeat repellat nisi inventore saepe est
-        repudiandae beatae non recusandae excepturi, qui consectetur error sequi
-        cum! Illum!
-      </p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque a porro blanditiis, id reprehenderit expedita ducimus, harum ipsum dicta odio veniam assumenda accusamus ipsam. Officia suscipit commodi libero deserunt fugit.</p>
     </main>
   );
 }
