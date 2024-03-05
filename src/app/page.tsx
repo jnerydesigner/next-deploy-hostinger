9de9400a6b1908958f293ef00393067655e6c13d
Jander Nery <jander.webmaster@gmail.com>
2024-03-05 15:08:51 -0400

    Texto novo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>New Deploy in NextJS - By Jander Nery</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque a porro blanditiis, id reprehenderit expedita ducimus, harum ipsum dicta odio veniam assumenda accusamus ipsam. Officia suscipit commodi libero deserunt fugit.</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque a
+        porro blanditiis, id reprehenderit expedita ducimus, harum ipsum dicta
+        odio veniam assumenda accusamus ipsam. Officia suscipit commodi libero
+        deserunt fugit.
+      </p>
     </main>
   );
 }
